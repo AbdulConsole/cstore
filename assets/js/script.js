8bfadaf93ee91a79784968a32b4181f7cd6271b9
AbdulConsole <hzwedt@gmail.com>
2024-01-18 02:45:43 +0100

    --

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,11 @@
-const products = {
-    1: {
-      id: 1,
-      name: "Body Oil",
-      price: "15.99",
-      url: "assets/img/p1.avif"
-    },
-    2: {
-      id: 2,
-      name: "Organic Cosmetics",
-      price: "17.99",
-      url: "assets/img/p2.avif"
-    },
-    3: {
-      id: 3,
-      name: "Elsa Ointment",
-      price: "14.99",
-      url: "assets/img/p3.avif"
-    },
-    4: {
-      id: 4,
-      name: "ILCE-7M2",
-      price: "11.99",
-      url: "assets/img/p4.avif"
-    },
-    5: {
-      id: 5,
-      name: "Cocooil",
-      price: "12.0",
-      url: "assets/img/p5.avif"
-    },
-    6: {
-      id: 6,
-      name: "Natural X",
-      price: "19.99",
-      url: "assets/img/p6.avif"
-    }
-  };
 let iconCart = document.querySelector('.icon-cart');
 let body = document.querySelector('body');
 let closeCart = document.querySelector('.close');
-// let listProductHTML = 
-
+// let addCart = document.querySelector('.addCart');
+const productContainer = document.querySelector("#container");
 let listProducts = [];
+
+
 
 iconCart.addEventListener('click', () => {
     body.classList.toggle('showCart');
@@ -49,6 +13,16 @@ iconCart.addEventListener('click', () => {
 closeCart.addEventListener('click', () => {
     body.classList.remove('showCart');
 })
+productContainer.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if (positionClick.classList.contains('addCart')) {
+        let product_id = positionClick.parentElement.dataset.id;
+        alert(product_id);
+        alert(parentElement);
+        // alert(1);
+    }
+})
+
 
 const initApp = () => {
     //get data from json file
@@ -56,7 +30,8 @@ const initApp = () => {
     .then(response => response.json())
     .then(data => {
         listProducts = data;
-        console.log(listProducts);
+        // console.log(listProducts);
+        displayProduct();
     })
 }
 
@@ -66,44 +41,41 @@ initApp();
 
 
 
+
+
+
+
+
+
 // A function to create html element for each products
 function createProductElement(product) {
     const productBox  = document.createElement("div");
     productBox.className = "product-box";
-
+    productBox.dataset.id = product.id;
     productBox.innerHTML = `
         <div class="card">
             <h1>${product.name}</h1>
-            <img src="${product.url}" alt="${product.name}">
+            <img src="${product.image}" alt="${product.name}">
             <div class="row">
                 <h2>&pound;${product.price}</h2>
-                <button class="add-to-cart" onclick="addToCart(${product.id})">Add to cart</button>
+                <button class="addCart">Add to cart</button>
             </div>
         </div>
     `;
 
     return productBox;
 }
-
 // A function to fetch the createProductEleent function and display it
 function displayProduct() {
-    const productContainer = document.getElementById("container");
-
     // Loop through all products object
-    for (const productId in products) {
-        if (products.hasOwnProperty(productId)) {
-            const productElement = createProductElement(products[productId]);
+    for (const productId in listProducts) {
+        if (listProducts.hasOwnProperty(productId)) {
+            const productElement = createProductElement(listProducts[productId]);
             productContainer.appendChild(productElement);
         }
     }
 }
-
 // A function to add product to cart
 function addToCart(productId) {
     console.log(`Product added to cart with ID: ${productId}`);
 }
-
-
-
-// Call the display function here
-window.onload = displayProduct;
